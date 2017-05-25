8578a8ae0bee40686731f8234b480bd4b9af5787
MorNando <chrismorland@lambtonsolutions.co.uk>
2017-05-25 22:39:59 +0100

    added start-sleep due to delay in terminal services starting

--- StartJump.ps1
+++ StartJump.ps1
@@ -2,4 +2,5 @@ import-module *azure*
 Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionName "Microsoft Partner Network"
 Start-AzureRmVM -Name jump -ResourceGroupName LABDEVELOPER
+start-sleep -Seconds 20
 mstsc /v:"jump.ukwest.cloudapp.azure.com"
